62ba1a022ba3dd0dd82a9e665dc7d5f9a8e5542a
mahiv87 <maherreraiv1987@gmail.com>
2024-06-16 12:18:24 -0600

    GET method test

--- src/main/java/com/notetaker/notetaker/note/NoteController.java
+++ src/main/java/com/notetaker/notetaker/note/NoteController.java
@@ -1,9 +1,22 @@
 package com.notetaker.notetaker.note;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
 
 @RestController
 @RequestMapping("api/notes")
 public class NoteController {
+    private final NoteService service;
+
+    public NoteController(NoteService service) {
+        this.service = service;
+    }
+
+    public ResponseEntity<List<Note>> findAll() {
+        List<Note> notes = service.findAll();
+        return ResponseEntity.ok().body(notes);
+    }
 }
