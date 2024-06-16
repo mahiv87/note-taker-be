b9a2fa649f1f3a3fc0b0444e3f21d8a959bfee7e
mahiv87 <maherreraiv1987@gmail.com>
2024-06-16 11:41:54 -0600

    init commit

---
+++ src/main/java/com/notetaker/notetaker/NotetakerApplication.java
@@ -0,0 +1,13 @@
+package com.notetaker.notetaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotetakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotetakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notetaker/notetaker/NotetakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notetaker.notetaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotetakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
