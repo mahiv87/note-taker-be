ec33ba5a69ce309e31ff0612c5aed0d06c493acc
mahiv87 <maherreraiv1987@gmail.com>
2024-06-16 12:28:38 -0600

    GET method debugged

--- src/main/java/com/notetaker/notetaker/note/NoteController.java
+++ src/main/java/com/notetaker/notetaker/note/NoteController.java
@@ -1,6 +1,7 @@
 package com.notetaker.notetaker.note;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ public class NoteController {
         this.service = service;
     }
 
+    @GetMapping
     public ResponseEntity<List<Note>> findAll() {
         List<Note> notes = service.findAll();
         return ResponseEntity.ok().body(notes);
