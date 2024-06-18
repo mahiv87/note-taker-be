c6bfc47931b783df2947980d48fd909794112a64
mahiv87 <maherreraiv1987@gmail.com>
2024-06-17 19:57:09 -0600

    cleaned up code

--- src/main/java/com/notetaker/notetaker/note/NoteRepository.java
+++ src/main/java/com/notetaker/notetaker/note/NoteRepository.java
@@ -1,8 +1,6 @@
 package com.notetaker.notetaker.note;
 
-import com.notetaker.notetaker.note.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 
 public interface NoteRepository extends JpaRepository<Note, Long> {
